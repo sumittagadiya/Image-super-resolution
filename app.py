7a492fc002730c937bc66669c318e222ce769998
sumittagadiya <tagadiyasumit@gmail.com>
2021-02-03 18:36:36 +0530

    app update

--- app.py
+++ app.py
@@ -14,33 +14,33 @@ app = Flask(__name__)
 
 ########################### PATH ###########################################
 # model architacture path
-app.config['MODEL_JSON_PATH'] = 'saved_model/edsr_model.json'
+MODEL_JSON_PATH = 'saved_model/edsr_model.json'
 # trained model weights
-app.config['MODEL_PATH_BICUBIC'] = 'saved_model/edsr_350000.h5'
-app.config['MODEL_PATH_UNKNOWN'] = 'saved_model/edsr_unknown_x4.h5'
-app.config['LETTER_SET'] = list(set('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'))
+MODEL_PATH_BICUBIC = 'saved_model/edsr_350000.h5'
+MODEL_PATH_UNKNOWN = 'saved_model/edsr_unknown_x4.h5'
+LETTER_SET = list(set('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'))
 # Uploaded image folder path
-app.config['UPLOAD_FOLDER'] = 'static/uploaded'
+UPLOAD_FOLDER = 'static/uploaded'
 # predicted image folder path
-app.config['PRED_FOLDER ']= 'static/predicted'
+PRED_FOLDER = 'static/predicted'
 
 ############################## Load pre-trained model #########################
-json_file = open(app.config['MODEL_JSON_PATH'], 'r')
+json_file = open(MODEL_JSON_PATH, 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = tf.keras.models.model_from_json(loaded_model_json)
-model.load_weights(app.config['MODEL_PATH_BICUBIC'])
+model.load_weights(MODEL_PATH_BICUBIC)
 # load unknown_x4 weights
 model1 = tf.keras.models.model_from_json(loaded_model_json)
-model1.load_weights(app.config['MODEL_PATH_UNKNOWN'])
+model1.load_weights(MODEL_PATH_UNKNOWN)
 print('Both Model Loaded succesfully')
 
 #################### generate random file name #######################
 def generate_random_name(filename):
     """ Generate a random name for an uploaded file. """
     ext = filename.split('.')[-1]
-    rns = [random.randint(0, len(app.config['LETTER_SET']) - 1) for _ in range(3)]
-    chars = ''.join([app.config['LETTER_SET'][rn] for rn in rns])
+    rns = [random.randint(0, len(LETTER_SET) - 1) for _ in range(3)]
+    chars = ''.join([LETTER_SET[rn] for rn in rns])
 
     new_name = "{new_fn}.{ext}".format(new_fn=chars, ext=ext)
     new_name = secure_filename(new_name)
@@ -99,7 +99,7 @@ def predicted():
         if str(request.form.get('models')) == 'edsr_unknown' :
             print('***** Using EDSR Unknown_x4 model weights ******')
             # Save the file to ./uploads
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'],image_file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER,image_file.filename)
             # save uploaded image
             image_file.save(file_path)
 
@@ -110,7 +110,7 @@ def predicted():
             # generate random image file name for predicted image
             pred_filename = generate_random_name(image_file.filename)
             # predicted image absolute path
-            pred_filepath = os.path.join(app.config['PRED_FOLDER '],pred_filename)
+            pred_filepath = os.path.join(PRED_FOLDER,pred_filename)
             # save predicted image
             pred_img.save(pred_filepath)
 
@@ -121,7 +121,7 @@ def predicted():
         #################### if model = bicubic_x4 #####################
 
         print('************ Using bicubic_x4 model weights **********')
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'],image_file.filename)
+        file_path = os.path.join(UPLOAD_FOLDER,image_file.filename)
         # save uploaded image
         image_file.save(file_path)
         # Make prediction
@@ -131,7 +131,7 @@ def predicted():
         # generate random image file name for predicted image
         pred_filename = generate_random_name(image_file.filename)
         # predicted image absolute path
-        pred_filepath = os.path.join(app.config['PRED_FOLDER '],pred_filename)
+        pred_filepath = os.path.join(PRED_FOLDER,pred_filename)
         # save predicted image
         pred_img.save(pred_filepath)
 
